e33f25a73484c621caf2caf6736ffd49b31df582
pellincr <44884108+pellincr@users.noreply.github.com>
2020-01-23 14:27:15 -0500

    Add files via upload

---
+++ Pelling-Craig-Mauro-Anthony-Soggluizzo-Ryan-q1.rkt
@@ -0,0 +1,113 @@
+#lang eopl
+(require test-engine/racket-tests)
+
+;Binary-search-tree ::= () | (Int Binary-search-tree Binary-search-tree)
+;path: int bst -> listof-symbols
+;Assumption: int n is an element of the given bst
+;Purpose: to return the list of left and rights showing how to find n in the given bst
+(define (path n bst)
+  (cond [(eq? (car bst) n) '()]
+        [(> (car bst) n) (cons 'left (path n (cadr bst)))]
+        [else (cons 'right (path n (caddr bst)))]))
+
+(check-expect (path 17 '(17 () ())) '())
+(check-expect (path 20 '(15 (10 () ()) (20 () ()))) '(right))
+(check-expect (path 17 '(14 (7 () (12 () ()))
+                            (26 (20 (17 () ()) ()) (31 () ())))) '(right left left))
+
+
+
+
+;Binary-Tree ::== leaf | (interior-node bin-tree bin-tree)
+;Interior-node ::== symbol
+
+;leaf: num -> bin-tree
+;purpose: to create a leaf for a bintree
+(define (leaf num)
+  (if (number? num) num
+  (eopl:error "Given value ~s is not a number" num)))
+
+;interior-node: symbol bin-tree bin-tree -> bin-tree
+;purpose: to create an interior node for a bin-tree
+(define (interior-node sym lchild rchild)
+  (if (symbol? sym)
+      (list sym lchild rchild)
+  (eopl:error "Given value ~s is not a symbol" sym)))
+
+;leaf?: leaf -> boolean
+;Purpose: to determine if the given value is a leaf
+(define (leaf? l)
+  (number? l))
+
+(check-expect (leaf? (leaf 5)) #t)
+(check-expect (leaf? 'hi) #f)
+
+
+;lson: interior-node -> bin-tree
+;Purpose: to get the left child of the given interior node
+(define (lson inode)
+  (cadr inode))
+
+(check-expect (lson (interior-node 's (leaf 10) (leaf 11))) 10)
+
+;rson: interior-node -> bin-tree
+;Purpose: to get the right child of the given interior node
+(define (rson inode)
+  (caddr inode))
+
+(check-expect (rson (interior-node 's (leaf 10) (leaf 11))) 11)
+
+;contents-of: interior-node -> symbol
+;Purpose: to get the node value of the given interior node
+(define (contents-of inode)
+  (if (leaf? inode) inode
+      (car inode)))
+
+(check-expect (contents-of (leaf 14)) 14)
+(check-expect (contents-of (interior-node 's (leaf 10) (leaf 11))) 's)
+
+
+
+;number-leaves: bintree -> bintree
+;purpose: to replace the leaves with the number of leaf it is in the tree
+(define (number-leaves btree)
+  ;INVENTORY
+  ;(leaf? btree)-determines if the bintree is a leaf
+  ;(lson btree) -returns the left child of the given btree
+  ;(rson btree) -returns the right child of the given btree
+  ;(contents-of btree) - returns the current value of the interior node
+  (number-leaves-helper btree 0))
+
+;number-leaves-helper: bintree accum -> bintree
+;purpose: to replace the leaves with the number of leaf it is in the tree
+;ACCUM INV:
+(define (number-leaves-helper btree ctr)
+  ;INVENTORY
+  ;(leaf? btree)-determines if the bintree is a leaf
+  ;(lson btree) -returns the left child of the given btree
+  ;(rson btree) -returns the right child of the given btree
+  ;(contents-of btree) - returns the current value of the interior node
+  (cond [(leaf? btree) ctr]
+        
+        [else (append (car btree)
+                      (number-leaves-helper (lson btree) ctr)
+                      (number-leaves-helper (rson btree) (+ 1 ctr)))]))
+
+
+
+;start from the top, if (Leaf? lson) then recurse with ctr plus one. Else recurse without increasing
+
+;(foo (bar 26 12) (baz 11 (quux 117 14)))
+;(check-expect (number-leaves
+;                (interior-node ’foo
+;                               (interior-node ’bar
+;                                              (leaf 26)
+;                                              (leaf 12))
+;                               (interior-node ’baz
+;                                              (leaf 11)
+;                                              (interior-node ’quux
+;                                                             (leaf 117)
+;                                                             (leaf 14)))))
+;              '(foo (bar 0 1) (baz 2 (quux 3 4))))
+;
+(test)
